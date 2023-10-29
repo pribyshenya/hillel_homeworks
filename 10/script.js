f2552d577005ae89278d357eca3bcbcb5e637733
Sergei Pribyshenya <82205829+pribyshenya@users.noreply.github.com>
2023-10-29 21:40:49 +0200

    Add files via upload

---
+++ 10/script.js
@@ -0,0 +1,28 @@
+// Зміна кольору тексту
+let textElement = document.getElementById("text");
+let colorChangeButton = document.getElementById("color-change-button");
+let isTextColorChanged = false;
+
+colorChangeButton.addEventListener("click", () => {
+  if (isTextColorChanged) {
+    textElement.style.color = "black";
+  } else {
+    textElement.style.color = "red";
+  }
+  isTextColorChanged = !isTextColorChanged;
+});
+
+let pifagorTable = document.getElementById("pifagor-table");
+for (let i = 1; i <= 10; i++) {
+  let row = pifagorTable.insertRow();
+  for (let j = 1; j <= 10; j++) {
+    let cell = row.insertCell();
+    cell.textContent = i * j;
+  }
+}
+
+let imageElement = document.getElementById("image");
+let randomImageIndex = Math.floor(Math.random() * 9) + 1;
+let imagePath = `images/${randomImageIndex}.jpg`;
+
+imageElement.src = imagePath;
